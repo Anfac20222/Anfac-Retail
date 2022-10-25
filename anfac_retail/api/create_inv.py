908319c2858a8d6d4dc80a01aa895829971b4f56
Ubuntu <ubuntu@ip-172-31-42-99.us-east-2.compute.internal>
2022-10-25 06:54:39 +0000

    Purchase

--- anfac_retail/api/create_inv.py
+++ anfac_retail/api/create_inv.py
@@ -69,3 +69,78 @@ def create_inv(doc_name ,dt , is_sales_return = False , is_credit = False):
     # cash_sales.save()
     
     
+
+
+
+
+def purchase(doc_name ,dt , is_purchase_return = False , is_credit = False):
+    cash_sales = frappe.get_doc(dt , doc_name)
+    supplier = cash_sales.supplier
+    # frappe.msgprint(order_doc.name)
+    items = []
+    empty_items = ""
+    for item in cash_sales.items:
+        # item_qty = frappe.db.get_value("Batch" , {"item" :item.item  }, "batch_qty" )
+        # if float(item_qty) <= 0 :
+        #     empty_items += item.item + ' , '
+        # #     frappe.throw(
+        # #     title='Ogerysiis',
+            
+        # #     exc=FileNotFoundError
+        # # )
+        qty = item.qty
+        if is_purchase_return:
+            qty = float(item.qty) * (-1)
+        items.append({
+            "item_code" : item.item,
+            "rate" : item.rate,
+            "qty" : qty
+        })
+    payments = []
+    # is_pos = 0
+    # if not is_credit:
+    # b =frappe.defaults.get_user_permissions(frappe.session.user)
+    # pos =b['POS Profile'][0].doc
+    # pos = frappe.get_doc("POS Profile" , pos)
+    # payment = pos.payments
+    
+    paid_amount = cash_sales.grand_total
+    if is_purchase_return:
+        paid_amount = cash_sales.grand_total * (-1)
+    is_paid = 0
+    account = ''
+    if not is_credit:
+        is_paid = 1
+        account = cash_sales.account
+        
+                    
+        
+                    
+          
+    is_return = 0
+    if is_purchase_return :
+         is_return = 1 
+
+    pur_doc = frappe.get_doc({
+        "doctype" : "Purchase Invoice",
+        "is_return" : is_return,
+        "supplier": supplier,
+        "discount_amount" : cash_sales.discount,
+        "is_paid" : is_paid,
+        "discount_amount" : cash_sales.discount,
+        "cash_bank_account" : account,
+        "paid_amount" : paid_amount,
+        # "payments": payments,
+       
+        "items" : items,
+       
+    })
+   
+    pur_doc.save()
+    pur_doc.submit()
+    frappe.msgprint('Billed successfully')
+    return pur_doc
+    # cash_sales.sales_invoice = sales_doc.name
+    # cash_sales.save()
+    
+    
