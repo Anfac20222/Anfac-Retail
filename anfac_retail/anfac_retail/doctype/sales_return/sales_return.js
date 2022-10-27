b319e9be41f535ffc57dd027f8722504aca225ae
Anfac2022 <anfactech2022@gmail.com>
2022-10-27 07:16:44 +0000

    sales reufnd

--- anfac_retail/anfac_retail/doctype/sales_return/sales_return.js
+++ anfac_retail/anfac_retail/doctype/sales_return/sales_return.js
@@ -1,9 +1,70 @@
 // Copyright (c) 2022, Anfac Tech and contributors
 // For license information, please see license.txt
-
+var d = {}
 frappe.ui.form.on('Sales Return', {
-	// refresh: function(frm) {
+	
+	refresh: function(frm) {
+		if(frm.is_new()){
+			frm.disable_save();
+		
+		
+		frm.add_custom_button('Save', () => {
+			
+			d = new frappe.ui.Dialog({
+				title: 'Return Type',
+				fields: [
+					{
+						label: 'Refund',
+						fieldname: 'refund',
+						fieldtype: 'Check',
+						read_only : 1,
+						default : 1
+						// onchange: function(e) {
+						// 	// alert("Selected : ", this.value)
+						// 	// console.log('this is ' , this.value)
+						// 	// check_uncheck(e)
+						// 	d.set_value('invoice' , !this.value)
 
+							
+						// }
+					},
+					{
+						label: 'Invoice',
+						fieldname: 'invoice',
+						fieldtype: 'Check',
+						onchange: function(e) {
+							// alert("Selected : ", this.value)
+							// console.log('this is ' , this.value)
+							// check_uncheck(e)
+							
+							d.set_value('refund' , !this.value)
+
+							
+						}
+					}
+				],
+				primary_action_label: 'Submit',
+				primary_action(values) {
+					// console.log(values);
+					frm.set_value('invoice' , values.invoice)
+					frm.set_value('refund' , values.refund)
+					frm.save()
+					frm.save("Submit")
+					d.hide();
+				}
+			});
+			
+			d.show();
+			
+			// frappe.set_route('Form', frm.doc.reference_type, frm.doc.reference_name);
+		})
+	}
+		
+	},
+		
+		// save:function(frm){
+			
+		// },
 		before_save:function(frm){
 			// alert('ok')
 			calculate_total(frm)
@@ -84,3 +145,9 @@ frappe.ui.form.on('Sales Return', {
 		frm.set_value('grand_total' , grand_total)
 	}
 	
+
+	function check_uncheck(e){
+			alert(d)
+			d.set_value("inovice" , 1)
+			console.log(d)
+	}
--- anfac_retail/anfac_retail/doctype/sales_return/sales_return.py
+++ anfac_retail/anfac_retail/doctype/sales_return/sales_return.py
@@ -8,7 +8,7 @@ from frappe.model.document import Document
 class SalesReturn(Document):
 	def on_submit(self):
 		# frappe.msgprint("ok")
-		sales_doc = create_inv(self.name ,dt = 'Sales Return' , is_sales_return = True)
+		sales_doc = create_inv(self.name ,dt = 'Sales Return' , is_sales_return = True , is_credit = self.invoice)
 		self.sales_invoice = sales_doc.name
 		self.save()
 	def on_cancel(self):
